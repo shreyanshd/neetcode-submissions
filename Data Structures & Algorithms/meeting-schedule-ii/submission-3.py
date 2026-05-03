35d1bdf327c1687fac172c535b2b30ef1ea703ff
Shreyansh Doshi <shreyansh.doshi95@gmail.com>
2026-05-03 12:04:41 +0530

    Add: meeting-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-3.py
@@ -0,0 +1,21 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        points = [(i.start, True) for i in intervals] + [(i.end, False) for i in intervals]
+        points.sort(key = lambda x: (x[0], x[1]))
+        print(points)
+        rooms, minRooms = 0, 0
+        for p in points:
+            add = 1 if p[1] else -1
+            rooms += add
+            minRooms = max(rooms, minRooms)
+        return minRooms
+
+
