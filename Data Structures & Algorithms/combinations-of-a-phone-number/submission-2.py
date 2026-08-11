55196d1c0459ac6bc4ed4364cee2ae910e48fca2
Shreyansh Doshi <shreyansh.doshi95@gmail.com>
2026-08-12 01:27:08 +0530

    Add: combinations-of-a-phone-number - submission-2

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        mapping = {
+            2: "abc",
+            3: "def",
+            4: "ghi",
+            5: "jkl",
+            6: "mno",
+            7: "pqrs",
+            8: "tuv",
+            9: "wxyz"
+        }
+
+        result = []
+        stack = []
+
+        def dfs(i):
+            if i == len(digits):
+                if stack:
+                    result.append(''.join(stack))
+                return
+            
+            digit = int(digits[i])
+            for c in mapping[digit]:
+                stack.append(c)
+                dfs(i + 1)
+                stack.pop()
+        
+        dfs(0)
+        return result
